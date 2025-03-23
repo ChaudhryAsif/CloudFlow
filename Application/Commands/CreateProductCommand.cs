98a5ed4b2fa42f0abaf47b3c69fd713c5f8d7016
Asif <ma232505@gmail.com>
2025-03-24 00:07:14 +0500

    Add CQRS design pattern

---
+++ Application/Commands/CreateProductCommand.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities;
+using MediatR;
+
+namespace Application.Commands
+{
+    public class CreateProductCommand : IRequest<Product>
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ Application/Commands/CreateProductCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Commands
+{
+    public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, Product>
+    {
+        private readonly IProductRepository _repository;
+
+        public CreateProductCommandHandler(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Product> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            var product = new Product { Id = Guid.NewGuid(), Name = request.Name, Price = request.Price };
+            await _repository.AddAsync(product);
+            return product;
+        }
+    }
+}
